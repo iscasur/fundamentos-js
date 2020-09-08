598bd7995e598c07c61ad03851df58558fdf7cde
Israel Castro <israel.castro@gmail.com>
2020-09-08 15:10:13 -0500

    [Add] sequence of numbers

--- js/index.js
+++ js/index.js
@@ -4,16 +4,30 @@ const orange = document.getElementById("orange");
 const green = document.getElementById("green");
 const btnStart = document.getElementById("btnStart");
 
-class Juego {
+class Game {
   constructor() {
     this.initialize();
+    this.generateSequence();
   }
 
   initialize() {
     btnStart.classList.add("hide");
+    this.level = 1;
+    this.colors = {
+      blue,
+      violet,
+      orange,
+      green,
+    };
+  }
+
+  generateSequence() {
+    this.sequence = new Array(10)
+      .fill(0)
+      .map((n) => Math.floor(Math.random() * 4));
   }
 }
 
 function startGame() {
-  var juego = new Juego();
+  window.game = new Game();
 }
